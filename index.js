832a1a075918059a0e81f79c61f3a11ac523c52f
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-01-17 23:45:09 +0600

    git commit

--- index.js
+++ index.js
@@ -3,9 +3,53 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
+// const password = process.env.USER_PASSWORD;
+// console.log(password);
+// mondoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.USER_PASSWORD}@cluster0.pkfik7i.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const blogCollection = client.db("OrganDB").collection("blogs");
+
+    //
+
+    app.post("/blogs", async (req, res) => {
+      const blogsData = req.body;
+      const result = await blogCollection.insertOne(blogsData);
+      res.send(result);
+    });
+    app.get("/blogs", async (req, res) => {
+      const result = await blogCollection.find().toArray()
+      res.send(result)
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("server is running");
@@ -14,3 +58,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+// 
